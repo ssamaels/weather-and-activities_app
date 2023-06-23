d9835d5f1f7c2e550beb8a7afb2ac302bca9526a
alex-ar10 <roehr.alexander@gmail.com>
2023-06-23 17:41:47 +0200

    start on form feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import useLocalStorage from "use-local-storage";
 import Weather from "./components/Weather/Weather.js";
 import Activities from "./components/Activities/Activities.js";
 import Form from "./components/Form/Form.js";
 import "./App.css";
 
 function App() {
+  const [activities, setActivities] = useLocalStorage("activity: ", "");
   return (
     <>
       <Weather />
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,1 +1,27 @@
-export default function Form() {}
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      name: formData.get("name"),
+      isForGoodWeather: formData.get("checkbox") === "on",
+    };
+    event.target.reset();
+    event.target.elements.name.focus();
+    onAddActivity(data);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h3>Add a new activity:</h3>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" id="activity-name" />
+        <label htmlFor="checkbox">Good-weather activity?</label>
+        <input type="checkbox" name="checkbox" id="checkbox" />
+        <button type="submit" name="submit" id="submit">
+          SUBMIT
+        </button>
+      </form>
+    </>
+  );
+}

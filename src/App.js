cacdfb3e2e00ad763d02dc0a39572f1d671320ea
alex-ar10 <roehr.alexander@gmail.com>
2023-06-23 16:50:37 +0200

    add basic file/ component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather from "./components/Weather/Weather.js";
+import Activities from "./components/Activities/Activities.js";
+import Form from "./components/Form/Form.js";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Weather />
+      <Activities />
+      <Form />
+    </>
   );
 }
 

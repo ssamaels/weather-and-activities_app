f40c825548d6b58b176afeadb5287edc6702ee85
alex-ar10 <roehr.alexander@gmail.com>
2023-06-26 14:04:24 +0200

    add fetch feature and fix filter feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useLocalStorage from "use-local-storage";
 import Weather from "./components/Weather/Weather.js";
 import Activities from "./components/Activities/Activities.js";
+import FilterActivities from "./components/FilterActivities/FilterActivities.js";
 import Form from "./components/Form/Form.js";
 import "./App.css";
 
 function App() {
   const [activities, setActivities] = useLocalStorage("Activity: ", []);
   const [isGoodWeather, setIsGoodWeather] = useState(true);
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(
+          "https://example-apis.vercel.app/api/weather"
+        );
+        const data = await response.json();
+        setIsGoodWeather(data.isGoodWeather);
+        setWeather(data);
+      } catch (error) {
+        console.log("Error fetching weather data:", error);
+      }
+    };
+
+    fetchWeather();
+    const interval = setInterval(fetchWeather, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   const handleAddActivity = (newActivity) => {
     setActivities((prevActivities) => [...prevActivities, newActivity]);
@@ -25,7 +46,11 @@ function App() {
 
   return (
     <>
-      <Weather />
+      <Weather weather={weather} />
+      <FilterActivities
+        isGoodWeather={isGoodWeather}
+        setIsGoodWeather={setIsGoodWeather}
+      />
       <Activities
         activities={filteredActivities}
         onDeleteActivity={handleDeleteActivity}
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,1 +1,12 @@
-export default function Weather() {}
+import React from "react";
+
+const Weather = ({ weather }) => {
+  return (
+    <div>
+      <h1>{weather.condition}</h1>
+      <h2>{weather.temperature}°C</h2>
+    </div>
+  );
+};
+
+export default Weather;

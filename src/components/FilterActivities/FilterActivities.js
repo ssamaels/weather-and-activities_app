f696ccb8d0fd5c87654a35fd6bc256cffd4de7aa
alex-ar10 <roehr.alexander@gmail.com>
2023-06-27 10:08:52 +0200

    add text depending on weather state and activities length

--- src/components/FilterActivities/FilterActivities.js
+++ src/components/FilterActivities/FilterActivities.js
@@ -4,14 +4,16 @@ const FilterActivities = ({ isGoodWeather, setIsGoodWeather }) => {
   // HTML code for the button filtering either the good or bad weather activities
   return (
     // a click sets the isGoodWeather state to it's opposite - note that the rerender of the fetch in App.js will change the state of the variable to the API data
-    <button
-      className="showButton"
-      onClick={() => setIsGoodWeather(!isGoodWeather)}
-    >
-      {isGoodWeather
-        ? "Show Bad Weather Activities"
-        : "Show Good Weather Activities"}
-    </button>
+    <>
+      <button
+        className="showButton"
+        onClick={() => setIsGoodWeather(!isGoodWeather)}
+      >
+        {isGoodWeather
+          ? "Show Bad Weather Activities"
+          : "Show Good Weather Activities"}
+      </button>
+    </>
   );
 };
 
